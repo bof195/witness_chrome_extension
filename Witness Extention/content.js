63c696f879a6e796cb51cc2552c4b112358d14d2
ben_t44 <bethomas@westmont.edu>
2020-02-19 12:29:23 -0800

    initial commit

---
+++ Witness Extention/content.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener(function (request) {
+    var images = document.getElementsByTagName('img'); 
+var srcList = [];
+for(var i = 0; i < images.length; i++) {
+    srcList.push(images[i].src);
+}
+console.log(srcList);
+alert("You witnessed " + srcList.length + " images");
+})
+
+// https://stackoverflow.com/questions/9321863/javascript-function-to-get-all-images-in-html
